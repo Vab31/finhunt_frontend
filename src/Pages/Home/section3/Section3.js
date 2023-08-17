4a201909b11fe68b981ced4c283102ba2693526d
Vaibhav Singh <singhvaibhav654@gmail.com>
2023-08-18 00:05:58 +0530

    section 4 responsive

--- src/Pages/Home/section3/Section3.js
+++ src/Pages/Home/section3/Section3.js
@@ -4,8 +4,8 @@ import './section3.css'
 
 const Section3 = () => {
   return (
-    <div className='section3 mt-0'>
-    <h1 className='section3_text'>On A Mission To Make India Financially Literate </h1>
+    <div className='mt-0'>
+    <h1 className=' mb-2'>On A Mission To Make India Financially Literate </h1>
     </div>
   )
 }
