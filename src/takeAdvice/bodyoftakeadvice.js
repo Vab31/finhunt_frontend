66ec3f9af54b5ff0b458d9c70f122c9f675dcbaf
Vaibhav Singh <singhvaibhav654@gmail.com>
2023-08-28 23:27:34 +0530

    newupdateuicore

--- src/takeAdvice/bodyoftakeadvice.js
+++ src/takeAdvice/bodyoftakeadvice.js
@@ -1,63 +1,63 @@
-import React from 'react';
-import '../font/style.css';
-import Toast from 'react-bootstrap/Toast';
+// import React from 'react';
+// import '../font/style.css';
+// import Toast from 'react-bootstrap/Toast';
 
-export default function BodyofAdvice() {
-  return (
-    <div>
-      <div className='flex'>
-      <div className='bg-gray-600 max-h-screen md:w-96 hidden md:block'>
-  <h1 className='text-white text-xl m-3 bg-slate-800 p-2 rounded' style={{ fontFamily: 'Instagram Sans Medium' }}>Most Asked Question</h1>
-  <ul style={{ listStyleType: 'Disc', fontFamily: 'Instagram Sans Medium' }} className='text-slate-200 m-2'>
-    <li>What should be the path to learn web development</li>
-    <li>What should be the path to learn web development</li>
-    <li>What should be the path to learn web development</li>
-    <li>What should be the path to learn web development</li>
-  </ul>
-</div>
+// export default function BodyofAdvice() {
+//   return (
+//     <div>
+//       <div className='flex'>
+//       <div className='bg-gray-600 max-h-screen md:w-96 hidden md:block'>
+//   <h1 className='text-white text-xl m-3 bg-slate-800 p-2 rounded' style={{ fontFamily: 'Instagram Sans Medium' }}>Most Asked Question</h1>
+//   <ul style={{ listStyleType: 'Disc', fontFamily: 'Instagram Sans Medium' }} className='text-slate-200 m-2'>
+//     <li>What should be the path to learn web development</li>
+//     <li>What should be the path to learn web development</li>
+//     <li>What should be the path to learn web development</li>
+//     <li>What should be the path to learn web development</li>
+//   </ul>
+// </div>
 
-<div className='bg-gray-700 h-96 w-screen max-h-screen justify-center items-center '>
-        <div className='bg-black'>
-        <div className=' flex justify-center  '>
-          <div className='flex flex-col items-center bg'>
-            <div>
-              <input
-                type='textarea'
-                placeholder='  Write on the wall'
-                className='mt-4 inline-block md:w-96 h-12 rounded-xl'
-              />
-              <button
-                type='submit'
-                style={{ fontFamily: 'Instagram Sans Medium' }}
-                className='ml-4 mt-4 p-2 bg-slate-500 rounded-md text-slate-100 text-xl'
-              >
-                Ask
-              </button>
-            </div>
-          </div>
+// <div className='bg-gray-700 h-96 w-screen max-h-screen justify-center items-center '>
+//         <div className='bg-black'>
+//         <div className=' flex justify-center  '>
+//           <div className='flex flex-col items-center bg'>
+//             <div>
+//               <input
+//                 type='textarea'
+//                 placeholder='  Write on the wall'
+//                 className='mt-4 inline-block md:w-96 h-12 rounded-xl'
+//               />
+//               <button
+//                 type='submit'
+//                 style={{ fontFamily: 'Instagram Sans Medium' }}
+//                 className='ml-4 mt-4 p-2 bg-slate-500 rounded-md text-slate-100 text-xl'
+//               >
+//                 Ask
+//               </button>
+//             </div>
+//           </div>
 
 
         
          
-        </div>
+//         </div>
 
 
-        <div className="border-t border-gray-300 my-4"></div>
+//         <div className="border-t border-gray-300 my-4"></div>
 
-</div>
-        <div className='flex justify-center items-center'>
-            <Toast>
-              <Toast.Header>
-                <strong className="me-auto">Bootstrap</strong>
-                <small>11 mins ago</small>
-              </Toast.Header>
-              <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-            </Toast>
-          </div>
+// </div>
+//         <div className='flex justify-center items-center'>
+//             <Toast>
+//               <Toast.Header>
+//                 <strong className="me-auto">Bootstrap</strong>
+//                 <small>11 mins ago</small>
+//               </Toast.Header>
+//               <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+//             </Toast>
+//           </div>
 
-</div>
+// </div>
 
-      </div>
-    </div>
-  );
-}
+//       </div>
+//     </div>
+//   );
+// }
--- src/takeAdvice/profilepage.js
+++ src/takeAdvice/profilepage.js
@@ -1,38 +1,38 @@
-import React from 'react'
-import BodyofAdvice from './bodyoftakeadvice'
-import EditIcon from '@mui/icons-material/Edit';
+// import React from 'react'
+// import BodyofAdvice from './bodyoftakeadvice'
+// import EditIcon from '@mui/icons-material/Edit';
 
-export default function Profilepage() {
-  return (
-  <div className='bg-gray-800'>
-      <div className='flex flex-wrap p-10  justify-center items-center'>
- <div className=''>
-  <img
-    className="object-cover object-center rounded h-60 w-60 md:h-30 md"
-    alt="hero"
-    src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
-  />
+// export default function Profilepage() {
+//   return (
+//   <div className='bg-gray-800'>
+//       <div className='flex flex-wrap p-10  justify-center items-center'>
+//  <div className=''>
+//   <img
+//     className="object-cover object-center rounded h-60 w-60 md:h-30 md"
+//     alt="hero"
+//     src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+//   />
 
-</div>
-<div  className='md:ml-40 text-center'>
+// </div>
+// <div  className='md:ml-40 text-center'>
     
-               <p className='text-slate-400 text-5xl' style={{fontFamily:'Intagram Sans Light'}}>Vaibhav Singh   <button className='rounded-full' > <a href='/editProfile'> <EditIcon/> </a></button> </p>
-               <p className='text-white'>@Vab_31</p>
-              {/* <button className=" text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg m-2">Edit profile</button> */}
-              {/* <button className=" text-white bg-indigo-500 border-0  py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg m-2">Button</button> */}
-               <br/>
-               {/* Normal user + user Profile */}
-               <button className=" text-white bg-indigo-500 border-0 py-2 px-6 m-2 rounded ">14K People Like</button>
-              <button className=" text-white bg-indigo-500 border-0  py-2 px-6 m-2 rounded ">50 Talks</button>
-              <button className=" text-white bg-indigo-500 border-0  py-2 px-6 m-2 rounded ">Make Insiparation</button>
-              <button className=" text-white bg-indigo-500 border-0  py-2 px-6 m-2 rounded ">Lets Talks</button>
-              <button className=" text-white bg-indigo-500 border-0  py-2 px-6 m-2 rounded ">₹40/hr</button>
-               <p className='text-white' style={{fontFamily:'Intagram Sans Light'}}> Computer Geeks Web development Expert Founder of Your Dost</p>
+//                <p className='text-slate-400 text-5xl' style={{fontFamily:'Intagram Sans Light'}}>Vaibhav Singh   <button className='rounded-full' > <a href='/editProfile'> <EditIcon/> </a></button> </p>
+//                <p className='text-white'>@Vab_31</p>
+//               {/* <button className=" text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg m-2">Edit profile</button> */}
+//               {/* <button className=" text-white bg-indigo-500 border-0  py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg m-2">Button</button> */}
+//                <br/>
+//                {/* Normal user + user Profile */}
+//                <button className=" text-white bg-indigo-500 border-0 py-2 px-6 m-2 rounded ">14K People Like</button>
+//               <button className=" text-white bg-indigo-500 border-0  py-2 px-6 m-2 rounded ">50 Talks</button>
+//               <button className=" text-white bg-indigo-500 border-0  py-2 px-6 m-2 rounded ">Make Insiparation</button>
+//               <button className=" text-white bg-indigo-500 border-0  py-2 px-6 m-2 rounded ">Lets Talks</button>
+//               <button className=" text-white bg-indigo-500 border-0  py-2 px-6 m-2 rounded ">₹40/hr</button>
+//                <p className='text-white' style={{fontFamily:'Intagram Sans Light'}}> Computer Geeks Web development Expert Founder of Your Dost</p>
               
-              </div>
+//               </div>
 
-</div>
-<BodyofAdvice/>
-</div>
-  )
-}
+// </div>
+// <BodyofAdvice/>
+// </div>
+//   )
+// }
